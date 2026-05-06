e5c377e220e8492433f54cf50e717c2c0711dd95
robertofabiot <rftercerom@uamv.edu.ni>
2026-05-06 16:19:11 -0600

    Aplicar SRP e inyeccion de dependencias

--- ArmadorMosaicos.py
+++ ArmadorMosaicos.py
@@ -30,137 +30,131 @@ class ArmadorMosaicos:
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
         # Acomodo para agarrar dos azules y dos verdes
         self.robot.seguidor_linea_distancia_desacelerado(self.sensor_color, 100, 52, margen_cm=5, tiempo_acomodo_ms=500)
-        self.robot.giro_preciso(-87)
+        self.robot.chasis.giro_preciso(-87)
         # Entrada
         self.robot.abrir_garra_delantera(170, velocidad=1000, wait_after=False)
-        self.robot.avanzar_recto(13, velocidad=1000, margen_cm=3)
+        self.robot.chasis.avanzar_recto(13, velocidad=1000, margen_cm=3)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
-        self.robot.avanzar_recto(-5)
+        self.robot.chasis.avanzar_recto(-5)
         self.robot.abrir_garra_delantera(170, velocidad=1000, wait_after=True, margen_grados=100)
-        self.robot.avanzar_recto(10, velocidad=1000)
+        self.robot.chasis.avanzar_recto(10, velocidad=1000)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
-        self.robot.avanzar_recto(-25, velocidad=1000)
+        self.robot.chasis.avanzar_recto(-25, velocidad=1000)
         self.robot.abrir_garra_delantera(100, velocidad=1000, margen_grados=20)
-        self.robot.avanzar_recto(6)
-        self.robot.avanzar_recto(-3.5)
+        self.robot.chasis.avanzar_recto(6)
+        self.robot.chasis.avanzar_recto(-3.5)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
 
 
-        self.robot.girar_sobre_eje(200, margen_grados=20)
-        self.robot.avanzar_recto(5, 1000, frenado=Stop.NONE)
+        self.robot.chasis.girar_sobre_eje(200, margen_grados=20)
+        self.robot.chasis.avanzar_recto(5, 1000, frenado=Stop.NONE)
         self.robot.seguidor_linea_color(self.sensor_color, 100, Color.BLUE, lado="izquierda", tiempo_acomodo_ms=0, distancia_cm=20)
         # Subida de garra
         self.robot.llevar_eje_central_al_tope("positivo", velocidad=400, limite_potencia=100)
         # Acomodo para que queden en su lugar
-        self.robot.mover_en_arco(-9, distancia_cm=3.6, stop=Stop.COAST)
-        self.robot.mover_en_arco(9.5, distancia_cm=2, stop=Stop.NONE) 
-        self.robot.avanzar_recto(7)
-        self.robot.mover_motor_izquierdo(30)
+        self.robot.chasis.mover_en_arco(-9, distancia_cm=3.6, stop=Stop.COAST)
+        self.robot.chasis.mover_en_arco(9.5, distancia_cm=2, stop=Stop.NONE) 
+        self.robot.chasis.avanzar_recto(7)
+        self.robot.chasis.mover_motor_izquierdo(30)
         # Soltar
         # 1. Bajar la garra hasta la altura de "jaula" (sin aplastar la impresión 3D)
         self.robot.mover_garra_trasera(-110)
         # 2. Abrir la garra delantera ligeramente para dar holgura a los bloques
         self.robot.abrir_garra_delantera(grados=50, velocidad=400) 
         # 3. ¡Vibrar!
-        self.robot.sacudir(iteraciones=3, potencia=60, tiempo_ms=100)
+        self.robot.chasis.sacudir(iteraciones=3, potencia=60, tiempo_ms=100)
         # 4. Soltar por completo y salir
         self.robot.abrir_garra_delantera_al_tope(velocidad=1200, limite_potencia=100) # Abre brazos
-        self.robot.sacudir(iteraciones=2, potencia=60, tiempo_ms=100)      
+        self.robot.chasis.sacudir(iteraciones=2, potencia=60, tiempo_ms=100)      
         # Acomodo para buscar los otros
-        self.robot.avanzar_recto(-30) # Retroceso limpio
+        self.robot.chasis.avanzar_recto(-30) # Retroceso limpio
         self.robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
-        self.robot.giro_preciso(155)
-        self.robot.avanzar_recto(28)
-        self.robot.mover_motor_izquierdo(120)
+        self.robot.chasis.giro_preciso(155)
+        self.robot.chasis.avanzar_recto(28)
+        self.robot.chasis.mover_motor_izquierdo(120)
         #Entrar
-        self.robot.avanzar_recto(12)
+        self.robot.chasis.avanzar_recto(12)
         # self.robot.giro_preciso_pd(90, kp=2.5)
-        self.robot.avanzar_recto(10, velocidad=1000)
+        self.robot.chasis.avanzar_recto(10, velocidad=1000)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
-        self.robot.avanzar_recto(-5)
+        self.robot.chasis.avanzar_recto(-5)
         self.robot.abrir_garra_delantera(170, velocidad=1000)
-        self.robot.avanzar_recto(10, velocidad=1000)
+        self.robot.chasis.avanzar_recto(10, velocidad=1000)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
-        self.robot.avanzar_recto(-25)
+        self.robot.chasis.avanzar_recto(-25)
         self.robot.abrir_garra_delantera(100, velocidad=1000, margen_grados=20)
-        self.robot.avanzar_recto(7)
-        self.robot.avanzar_recto(-3.5)
+        self.robot.chasis.avanzar_recto(7)
+        self.robot.chasis.avanzar_recto(-3.5)
         self.robot.cerrar_garra_delantera_al_tope(velocidad=1000, limite_potencia=100)
         #Agarrar
         #Buscar seguidor
-        self.robot.giro_preciso(80)
-        self.robot.avanzar_recto(32) #ANTES ERA 28 
-        self.robot.mover_motor_izquierdo(150)
+        self.robot.chasis.giro_preciso(80)
+        self.robot.chasis.avanzar_recto(32) #ANTES ERA 28 
+        self.robot.chasis.mover_motor_izquierdo(150)
         self.robot.seguidor_linea_distancia(self.sensor_color, 80, 20)
         self.robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
-        self.robot.mover_en_arco(9, distancia_cm=3.8, stop=Stop.COAST)
-        self.robot.mover_en_arco(-9, distancia_cm=3.8, stop=Stop.BRAKE) 
-        self.robot.avanzar_recto(14)
+        self.robot.chasis.mover_en_arco(9, distancia_cm=3.8, stop=Stop.COAST)
+        self.robot.chasis.mover_en_arco(-9, distancia_cm=3.8, stop=Stop.BRAKE) 
+        self.robot.chasis.avanzar_recto(14)
         self.robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
         self.robot.mover_eje_central(90)
         # 2 Abrir la garra delantera ligeramente para dar holgura a los bloques
         self.robot.abrir_garra_delantera(grados=50, velocidad=400) 
         self.robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
         # 3. ¡Vibrar!
-        self.robot.sacudir(iteraciones=4, potencia=60, tiempo_ms=100)
+        self.robot.chasis.sacudir(iteraciones=4, potencia=60, tiempo_ms=100)
         # 4. Soltar por completo y salir
         self.robot.abrir_garra_delantera_al_tope(velocidad=1200, limite_potencia=100) # Abre brazos
         self.robot.llevar_eje_central_al_tope(direccion="positivo", limite_potencia=100)
-        self.robot.avanzar_recto(-30) # Retroceso limpio
-
-        """
-        Estimado programador,
-        admiro tu valentía para meterte a arreglar esta mierda.
-        Buena suerte.
-        """
+        self.robot.chasis.avanzar_recto(-30) # Retroceso limpio
 
         """
         Acá se pretende agarrar los bloques azul y verde y dejarlos en la fila de atrás de la garra.
         """
-        self.robot.giro_preciso(180)
-        self.robot.avanzar_recto(40)
+        self.robot.chasis.giro_preciso(180)
+        self.robot.chasis.avanzar_recto(40)
         self.robot.abrir_garra_delantera_al_tope(170, limite_potencia=100)
-        self.robot.avanzar_recto(20)
+        self.robot.chasis.avanzar_recto(20)
         self.robot.cerrar_garra_delantera_al_tope()
-        self.robot.avanzar_recto(-10)
+        self.robot.chasis.avanzar_recto(-10)
         self.robot.abrir_garra_delantera(170, 1000)
-        self.robot.avanzar_recto(10)
-        self.robot.avanzar_recto(-5)
+        self.robot.chasis.avanzar_recto(10)
+        self.robot.chasis.avanzar_recto(-5)
         self.robot.cerrar_garra_delantera_al_tope()
         
         """"
         Acá retroceder y girar para agarrar los otros dos verdes
         """
-        self.robot.avanzar_recto(-10)
-        self.robot.giro_preciso(90)
-        self.robot.avanzar_recto(-15)
-        self.robot.mover_motor_derecho(180, 1000)
-        self.robot.avanzar_recto(10)
-        self.robot.mover_motor_izquierdo(180, 1000)
+        self.robot.chasis.avanzar_recto(-10)
+        self.robot.chasis.giro_preciso(90)
+        self.robot.chasis.avanzar_recto(-15)
+        self.robot.chasis.mover_motor_derecho(180, 1000)
+        self.robot.chasis.avanzar_recto(10)
+        self.robot.chasis.mover_motor_izquierdo(180, 1000)
         self.robot.abrir_garra_delantera(170, 1000)
-        self.robot.avanzar_recto(10)
+        self.robot.chasis.avanzar_recto(10)
         self.robot.cerrar_garra_delantera_al_tope(1000, 100)
 
         """Acá agarra los 2 bloques restantes con la garra de atras"""
-        self.robot.avanzar_recto(-30)
-        self.robot.giro_preciso(180)
+        self.robot.chasis.avanzar_recto(-30)
+        self.robot.chasis.giro_preciso(180)
         self.robot.llevar_eje_central_al_tope(direccion="positivo", limite_potencia=100)
-        self.robot.avanzar_recto(-30)
+        self.robot.chasis.avanzar_recto(-30)
         self.robot.llevar_eje_central_al_tope(direccion="negativo", limite_potencia=100)
 
         """Acá irlo a dejar al mosaico desde el espacio de los verdes"""
-        self.robot.avanzar_recto(30)
-        self.robot.giro_preciso(90)
+        self.robot.chasis.avanzar_recto(30)
+        self.robot.chasis.giro_preciso(90)
         self.robot.seguidor_linea_distancia(self.sensor_color, 1000, 200)
-        self.robot.giro_preciso(-90)
-        self.robot.avanzar_recto(30)
+        self.robot.chasis.giro_preciso(-90)
+        self.robot.chasis.avanzar_recto(30)
         self.robot.seguidor_linea_color(self.sensor_color, 1000, Color.GREEN, distancia_cm=40)
 
         """Acá el robot hp se acomoda para quedar recto con los colores"""
         self.robot.llevar_eje_central_al_tope("positivo", velocidad=400, limite_potencia=100)
-        self.robot.mover_motor_derecho(180, 1000)
-        self.robot.avanzar_recto(3)
-        self.robot.mover_motor_izquierdo(180, 1000)
+        self.robot.chasis.mover_motor_derecho(180, 1000)
+        self.robot.chasis.avanzar_recto(3)
+        self.robot.chasis.mover_motor_izquierdo(180, 1000)
 
         """Acá los deja"""
         # 1. Bajar la garra hasta la altura de "jaula" (sin aplastar la impresión 3D)
@@ -168,10 +162,10 @@ class ArmadorMosaicos:
         # 2. Abrir la garra delantera ligeramente para dar holgura a los bloques
         self.robot.abrir_garra_delantera(grados=50, velocidad=400) 
         # 3. ¡Vibrar!
-        self.robot.sacudir(iteraciones=3, potencia=60, tiempo_ms=100)
+        self.robot.chasis.sacudir(iteraciones=3, potencia=60, tiempo_ms=100)
         # 4. Soltar por completo y salir
         self.robot.abrir_garra_delantera_al_tope(velocidad=1200, limite_potencia=100) # Abre brazos
-        self.robot.sacudir(iteraciones=2, potencia=60, tiempo_ms=100)   
+        self.robot.chasis.sacudir(iteraciones=2, potencia=60, tiempo_ms=100)   
 
     def _armar_verde_amarillo(self):
         pass
--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def cemento_y_llana():
     Termina: dejando el bloque de cemento, con la garra hacia arriba, viendo hacia la pared de la mesa
     """
     # Acomodarse para seguidor
-    mi_robot.mover_en_arco(radio_cm=13, distancia_cm=15, stop=Stop.NONE)
+    mi_robot.chasis.mover_en_arco(radio_cm=13, distancia_cm=15, stop=Stop.NONE)
 
     # Seguidor de línea hasta cemento
     mi_robot.seguidor_linea_distancia(sensor, 100, distancia_cm=89, tiempo_acomodo_ms=0, margen_cm=10)
@@ -33,26 +33,26 @@ def cemento_y_llana():
     # Acomodarse para cemento
     mi_robot.giro_preciso_pd(-94, margen_grados=5)
     mi_robot.mover_garra_trasera(53, velocidad=1200, wait_after=False) 
-    mi_robot.avanzar_recto(-11, velocidad=1000, frenado=Stop.COAST, margen_cm=6)
+    mi_robot.chasis.avanzar_recto(-11, velocidad=1000, frenado=Stop.COAST, margen_cm=6)
     mi_robot.mover_eje_central(50)
 
 
     # Empujar la llana
-    mi_robot.avanzar_recto(11, velocidad=1000, frenado=Stop.BRAKE, margen_cm=7)
+    mi_robot.chasis.avanzar_recto(11, velocidad=1000, frenado=Stop.BRAKE, margen_cm=7)
     wait(1)
-    mi_robot.mover_motor_derecho(-490, velocidad=1000, margen_grados=0) 
+    mi_robot.chasis.mover_motor_derecho(-490, velocidad=1000, margen_grados=0) 
     wait(1)
-    mi_robot.avanzar_recto(-18, velocidad=1000, frenado=Stop.NONE, margen_cm=3) # Llana dejada
+    mi_robot.chasis.avanzar_recto(-18, velocidad=1000, frenado=Stop.NONE, margen_cm=3) # Llana dejada
 
     # Se acomoda para el siguiente seguidor de línea (Ambos arcos fluyen juntos)
-    mi_robot.mover_en_arco(-50, distancia_cm=20)
+    mi_robot.chasis.mover_en_arco(-50, distancia_cm=20)
     wait(1)
-    mi_robot.mover_en_arco(19, angulo=25)
+    mi_robot.chasis.mover_en_arco(19, angulo=25)
 
     mi_robot.seguidor_linea_distancia(sensor, 100, distancia_cm=69, tiempo_acomodo_ms=0, margen_cm=8)
 
     # Dejar cemento
-    mi_robot.mover_motor_derecho(-420, velocidad=1000, margen_grados=20)
+    mi_robot.chasis.mover_motor_derecho(-420, velocidad=1000, margen_grados=20)
     mi_robot.mover_eje_central(-70, velocidad=1000, margen_grados=20) # Cemento dejado
 
 def agarrar_bloques_blancos():
@@ -61,36 +61,36 @@ def agarrar_bloques_blancos():
     Termina: en el área de los bloques blancos, en posición de 45 grados viendo hacia afuera
     """
     # Sale del área de cemento para ir hacia los bloques blancos
-    mi_robot.mover_en_arco(-14, distancia_cm=20, stop=Stop.BRAKE)
+    mi_robot.chasis.mover_en_arco(-14, distancia_cm=20, stop=Stop.BRAKE)
 
     # Seguidor hasta el cuadro blanco
     mi_robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
     mi_robot.seguir_hasta_interseccion(sensor, sensor_trasero, 50, kp=0.6)
 
     # Acomodo para agarrar bloques blancos 
-    mi_robot.avanzar_recto(-10, velocidad=1000)
+    mi_robot.chasis.avanzar_recto(-10, velocidad=1000)
     mi_robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
-    mi_robot.giro_preciso(-175, margen_grados=5) #Antes no tenia margen
-    mi_robot.mover_motor_derecho(30)
-    mi_robot.avanzar_recto(-20, margen_cm=3) 
+    mi_robot.chasis.giro_preciso(-175, margen_grados=5) #Antes no tenia margen
+    mi_robot.chasis.mover_motor_derecho(30)
+    mi_robot.chasis.avanzar_recto(-20, margen_cm=3) 
 
     # Agarrar bloques
     mi_robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
 
 def dejar_bloques_blancos():
-    mi_robot.avanzar_recto(5, velocidad=1000, frenado=Stop.NONE)
-    mi_robot.motor_izquierda.run_angle(speed=1000, rotation_angle=370, then=Stop.NONE)
+    mi_robot.chasis.avanzar_recto(5, velocidad=1000, frenado=Stop.NONE)
+    mi_robot.chasis.mover_motor_izquierdo(370, velocidad=1000)
 
-    mi_robot.mover_en_arco(radio_cm=-114, angulo=24, stop=Stop.NONE)
+    mi_robot.chasis.mover_en_arco(radio_cm=-114, angulo=24, stop=Stop.NONE)
 
     # Seguidor hasta detectar el color verde
     mi_robot.seguidor_linea_color(sensor, 100, Color.GREEN, lado="derecha", distancia_cm=17)
 
     # Retrocede para no pegar en el mosaico
-    mi_robot.avanzar_recto(-0.5)
+    mi_robot.chasis.avanzar_recto(-0.5)
     # Dejar los bloques
     mi_robot.giro_eje_puro(218, kp=5 ,kd=10, min_speed=200)
-    mi_robot.avanzar_recto(-24, velocidad=1000, margen_cm=2)
+    mi_robot.chasis.avanzar_recto(-24, velocidad=1000, margen_cm=2)
     mi_robot.mover_garra_trasera(-80, margen_grados=20)
 
 def detectar_mosaico():
@@ -100,14 +100,14 @@ def detectar_mosaico():
             (el que se detecta de primero)
     """
     # Acomodarse para seguidor
-    mi_robot.avanzar_recto(14, velocidad=1000, frenado=Stop.NONE)
-    mi_robot.mover_motor_derecho(250, margen_grados=30)
+    mi_robot.chasis.avanzar_recto(14, velocidad=1000, frenado=Stop.NONE)
+    mi_robot.chasis.mover_motor_derecho(250, margen_grados=30)
 
     # Seguir línea por estabilidad
     mi_robot.seguidor_linea_distancia(sensor, 60, 15, tiempo_acomodo_ms=0, kp=0.45, kd=1.8, k_freno=0.8) 
 
     # Retroceder hasta el mosaico
-    mi_robot.avanzar_recto(-28 , velocidad=700) 
+    mi_robot.chasis.avanzar_recto(-28 , velocidad=700) 
 
     # Escaneo y acomodo de ser necesario
     mi_robot.mover_garra_trasera(45, frenado=Stop.HOLD)
@@ -115,7 +115,7 @@ def detectar_mosaico():
     mosaico = mi_robot.identificar_combinacion(sensor_trasero, -5)
     print(f"{mosaico}" if mosaico != -1 else "la cagaste") #Para ver en consola la combinación detectada
     if (mosaico == 1 or mosaico == 2): # Condición para acomodos
-        mi_robot.avanzar_recto(5)
+        mi_robot.chasis.avanzar_recto(5)
     return mosaico
 
 def agarrar_bloques_amarillos():
@@ -128,18 +128,18 @@ def agarrar_bloques_amarillos():
     mi_robot.mover_garra_trasera(-50)
     mi_robot.seguidor_linea_distancia(sensor, 100, 19)
     mi_robot.giro_preciso_pd(-55)
-    mi_robot.avanzar_recto(25)
+    mi_robot.chasis.avanzar_recto(25)
     mi_robot.giro_preciso_pd(45)
 
     # Seguir hasta el cuadro amarillo
     mi_robot.seguidor_linea_color(sensor, 100, Color.YELLOW, distancia_cm=26)
 
     # Acomodo para agarrar bloques amarillos
-    mi_robot.avanzar_recto(-10, velocidad=1000)
+    mi_robot.chasis.avanzar_recto(-10, velocidad=1000)
     mi_robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
-    mi_robot.giro_preciso(-175)
-    mi_robot.mover_motor_derecho(30)
-    mi_robot.avanzar_recto(-20) 
+    mi_robot.chasis.giro_preciso(-175)
+    mi_robot.chasis.mover_motor_derecho(30)
+    mi_robot.chasis.avanzar_recto(-20) 
 
     # Agarrar bloques
     mi_robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
@@ -150,9 +150,9 @@ def dejar_bloques_amarillos():
     Termina: dejando los bloques amarillos, viendo hacia la pared de la mesa
     """
     # Acomodarse para buscar la línea
-    mi_robot.mover_motor_derecho(225, velocidad=1000, margen_grados=30)
-    mi_robot.avanzar_recto(60, velocidad=1000, frenado=Stop.NONE)
-    mi_robot.mover_motor_izquierdo(225, velocidad=1000, margen_grados=30)
+    mi_robot.chasis.mover_motor_derecho(225, velocidad=1000, margen_grados=30)
+    mi_robot.chasis.avanzar_recto(60, velocidad=1000, frenado=Stop.NONE)
+    mi_robot.chasis.mover_motor_izquierdo(225, velocidad=1000, margen_grados=30)
 
     # Seguir la línea
     mi_robot.seguidor_linea_distancia(sensor, 100, distancia_cm=58 , lado="izquierda", tiempo_acomodo_ms=800)
@@ -160,7 +160,7 @@ def dejar_bloques_amarillos():
 
     # Dejar los bloques
     mi_robot.giro_preciso_pd(-90)
-    mi_robot.avanzar_recto(-17, velocidad=1000)
+    mi_robot.chasis.avanzar_recto(-17, velocidad=1000)
     mi_robot.mover_garra_trasera(-55)
 
 def recoger_bloques_azules():
@@ -169,7 +169,7 @@ def recoger_bloques_azules():
     Termina: con los bloques azules agarrados, listo para salir
     """
     # Acomodarse para seguidor de línea
-    mi_robot.avanzar_recto(15, velocidad=1000)
+    mi_robot.chasis.avanzar_recto(15, velocidad=1000)
     mi_robot.giro_preciso_pd(-90)
 
     # Seguir línea
@@ -177,18 +177,18 @@ def recoger_bloques_azules():
 
     # Buscar el siguiente seguidor
     mi_robot.giro_preciso_pd(-45)
-    mi_robot.avanzar_recto(13, velocidad=1000)
-    mi_robot.mover_motor_izquierdo(300)
+    mi_robot.chasis.avanzar_recto(13, velocidad=1000)
+    mi_robot.chasis.mover_motor_izquierdo(300)
 
     # Seguidor hasta el cuadro azul
     mi_robot.seguidor_linea_color(sensor, 100, Color.BLUE, distancia_cm=25)
 
     # Acomodo para agarrar bloques azules
-    mi_robot.avanzar_recto(-10)
+    mi_robot.chasis.avanzar_recto(-10)
     mi_robot.llevar_eje_central_al_tope("negativo", limite_potencia=100)
-    mi_robot.giro_preciso(-175)
-    mi_robot.mover_motor_derecho(30)
-    mi_robot.avanzar_recto(-20) 
+    mi_robot.chasis.giro_preciso(-175)
+    mi_robot.chasis.mover_motor_derecho(30)
+    mi_robot.chasis.avanzar_recto(-20) 
 
     # Agarrar bloques
     mi_robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
@@ -201,20 +201,20 @@ def dejar_bloques_azules_y_pala():
     """
     mi_robot.llevar_eje_central_al_tope("positivo", limite_potencia=100)
     mi_robot.giro_preciso_pd(-35)
-    mi_robot.avanzar_recto(52, velocidad=1100)
+    mi_robot.chasis.avanzar_recto(52, velocidad=1100)
 
     # Giro para salir con la pala
-    mi_robot.mover_motor_izquierdo(210)
-    mi_robot.avanzar_recto(-10)
-    mi_robot.giro_preciso(-180)
-    mi_robot.avanzar_recto(-10)
-    mi_robot.mover_motor_derecho(-150)
-    mi_robot.avanzar_recto(-46)
-    mi_robot.mover_motor_izquierdo(-120)
-    mi_robot.avanzar_recto(-80)
+    mi_robot.chasis.mover_motor_izquierdo(210)
+    mi_robot.chasis.avanzar_recto(-10)
+    mi_robot.chasis.giro_preciso(-180)
+    mi_robot.chasis.avanzar_recto(-10)
+    mi_robot.chasis.mover_motor_derecho(-150)
+    mi_robot.chasis.avanzar_recto(-46)
+    mi_robot.chasis.mover_motor_izquierdo(-120)
+    mi_robot.chasis.avanzar_recto(-80)
 
     # Acá deja la pala en el inicio
-    mi_robot.avanzar_recto(20, velocidad=1000)
+    mi_robot.chasis.avanzar_recto(20, velocidad=1000)
     mi_robot.giro_preciso_pd(-90)
 
     # Dejamos los bloques azules en un espacio accesible 
