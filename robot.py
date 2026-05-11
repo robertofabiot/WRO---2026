b8c33efb689d1a39b45035c44bde863f4890f60a
robertofabiot <rftercerom@uamv.edu.ni>
2026-05-11 15:56:08 -0600

    Update robot for 5 motors

--- robot.py
+++ robot.py
@@ -8,13 +8,16 @@ from Navegacion import Navegacion
 from Mecanismos import GarraDelantera, GarraTrasera
 
 class Robot:
-    def __init__(self, port_izq, port_der, port_garra_trasera, port_garra_delantera): 
+    def __init__(self, port_izq, port_der, port_garra_trasera, port_garra_delantera, port_pinza): 
         # 1. Hardware Base
         self.hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
         self.motor_izquierda = Motor(port_izq, Direction.COUNTERCLOCKWISE)
         self.motor_derecha = Motor(port_der, Direction.CLOCKWISE)
+        
+        # Motores de mecanismos
         self.motor_garra_trasera = Motor(port_garra_trasera, Direction.COUNTERCLOCKWISE)
-        self.motor_garra_delantera = Motor(port_garra_delantera)
+        self.motor_garra_delantera = Motor(port_garra_delantera) 
+        self.motor_pinza = Motor(port_pinza)                     
         
         # 2. Configuración de DriveBase
         self.drive_base = DriveBase(self.motor_izquierda, self.motor_derecha, config.DIAMETRO_RUEDA, config.SEPARACION_RUEDAS)
@@ -30,5 +33,6 @@ class Robot:
         self.navegacion = Navegacion(self.chasis)
         
         # Subsistemas actualizados
-        self.garra_delantera = GarraDelantera(self.motor_garra_delantera)
         self.garra_trasera = GarraTrasera(self.motor_garra_trasera)
+        # Le pasamos AMBOS motores a la garra delantera
+        self.garra_delantera = GarraDelantera(self.motor_garra_delantera, self.motor_pinza)
