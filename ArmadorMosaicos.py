30caad2ace6fce4b4b8c90c796c8d5cf153dc1e5
robertofabiot <rftercerom@uamv.edu.ni>
2026-07-29 12:00:04 -0600

    feat start of one mosaic and added a controller to test from phone

--- ArmadorMosaicos.py
+++ ArmadorMosaicos.py
@@ -1,7 +1,8 @@
 from pybricks.parameters import Color, Stop
+from robot import Robot
 
 class ArmadorMosaicos:
-    def __init__(self, robot_instancia, sensor_color):
+    def __init__(self, robot_instancia: Robot, sensor_color, prueba):
         self.robot = robot_instancia
         self.sensor_color = sensor_color
         
@@ -14,6 +15,12 @@ class ArmadorMosaicos:
             5: self._armar_blanco
         }
 
+        if prueba==True:
+            self.robot.garra_trasera.establecer_cero()
+            self.robot.garra_delantera.establecer_cero()
+            self.robot.garra_delantera.establecer_cero_pinza()
+            self.robot.garra_trasera.ir_a_porcentaje(97)
+
     def armar(self, numero_mosaico: int):
         rutina_a_ejecutar = self.rutinas.get(numero_mosaico, self._armar_verde_verde)
         print(f"Ejecutando rutina de armado para mosaico: {numero_mosaico}")
@@ -24,7 +31,29 @@ class ArmadorMosaicos:
         pass   
 
     def _armar_verde_amarillo(self):
-        pass
+        
+        self.robot.chasis.mover_motor_derecho(-600, encadenado=True)
+        self.robot.chasis.avanzar_recto(-2, velocidad=1000, encadenado=True)
+        self.robot.chasis.cuadrar_contra_pared(150, potencia=100, angulo_referencia=90)
+
+        self.robot.chasis.avanzar_recto(2, velocidad=1000, encadenado=True)
+        self.robot.navegacion.avanzar_contando_lineas(self.sensor_color, 2, Color.BLACK, distancia_extra_cm=5, encadenado=True, debug=False)
+
+        self.robot.navegacion.giro_absoluto_pd(0)
+        self.robot.chasis.avanzar_recto(12, velocidad=1000)
+        self.robot.garra_delantera.ir_a_porcentaje(90)
+
+        self.robot.chasis.avanzar_recto(-25)
+        self.robot.garra_delantera.ir_a_porcentaje(0)
+        self.robot.chasis.avanzar_recto(5)
+        self.robot.navegacion.giro_absoluto_motor_derecho(270)
+
+        self.robot.garra_delantera.ir_a_porcentaje_pinza(70, wait_after=False)
+        self.robot.navegacion.avanzar_contando_lineas(self.sensor_color, 2, Color.BLACK, tiempo_ciego_s=0.2, velocidad=-1000, velocidad_lenta=-150)
+        self.robot.navegacion.giro_absoluto_pd(0)
+        self.robot.garra_delantera.ir_a_porcentaje(90, velocidad=1000)
+        self.robot.chasis.avanzar_recto(5, velocidad=1000)
+        self.robot.garra_delantera.cerrar_al_tope(limite_potencia=80)
 
     def _armar_azul(self):
         pass
