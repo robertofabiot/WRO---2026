79a5f031143f4f55179158e4b39313fcce003a00
robertofabiot <rftercerom@uamv.edu.ni>
2026-07-29 10:03:09 -0600

    Feat new bateria baja sound

--- RevisadorBateria.py
+++ RevisadorBateria.py
@@ -9,8 +9,16 @@ class RevisadorBateria:
         bateria = self._obtener_bateria()
         bateria_minima = config.BATERIA_MINIMA
         print(f"Bateria actual = {bateria}")
+        
         if bateria < bateria_minima:
             print(f"Batería menor a {bateria_minima}")
+            
+            # Sonido "Gamer" de batería baja (versión mucho más grave)
+            self.robot.hub.speaker.beep(350, 150) # Tono medio-bajo
+            self.robot.hub.speaker.beep(250, 150) # Tono bajo
+            self.robot.hub.speaker.beep(150, 150) # Tono más bajo
+            self.robot.hub.speaker.beep(80, 600)  # Tono final muy grave (casi un zumbido)
+            
             continuar = input("¿Desea continuar? (y/n): ")
 
             return continuar == "y"
